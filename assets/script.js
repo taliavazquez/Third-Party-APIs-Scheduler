06e3c71446fc0133a3e5e78541bd366e586fc7b6
Talia Vazquez <66280398+taliavazquez@users.noreply.github.com>
2020-08-04 22:10:34 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,25 +4,48 @@ function setDate() {
   const date = moment().format("dddd, MMMM Do YYYY, h:mm a");
   todaysDate.html(date);
 }
+
+
+var currentHour=moment().hours();
+console.log(currentHour);
+
+
 function hourList() {
   var containerDiv = $(".container");
   //hours loop
   for (let i = 10; i <= 18; i++) {
     var currentDiv = $("<div " + "class=flex-container;'>" + "</div>");
-    if (i <= 11) var labelHour = $("<p id='hourLabel'>" + i + "00" + "</p>");
-    else var labelHour = $("<p id='hourLabel'>" + i + "00" + "</p>");
+    // removed if and else 
+    // var labelHour = $("<p id='hourLabel'>" + i + "00" + "</p>");
+    var labelHour = $("<p id='hourLabel'>" + i + ":00" + "</p>");
+    console.log(labelHour);
+    // input text user 
     var inputText = $("<input id=" + "txt" + i + " class='inputText'></input>");
     var saveBtn = $(
       "<button id=" + i + " class='buttonColor'>" + "Save" + "</button>"
     );
+    // if statement for condition "if the time has passed let it be this color yellowgreen with blue text"
+    // else if "the time is present let it be magenta and yellow"
+    // or else the default of " black/orange text"
+    if (i==currentHour) {
+      inputText.addClass('present');
+    } else if (i<currentHour) {
+      inputText.addClass('past');
+    // } else (i>currentHour)
+    //   inputText.addClass('future');
+    };
+    console.log(currentHour);
     currentDiv.append(labelHour);
     currentDiv.append(inputText);
     currentDiv.append(saveBtn);
     containerDiv.append(currentDiv);
   }
+
   //after creating the elements, fill out with the input text
   displayStorage();
 }
+
+
 //pull/edit/push (w/ parameters)
 function saveStorage(index, textInput) {
   //pull/declare
@@ -35,6 +58,8 @@ function saveStorage(index, textInput) {
   localStorage.setItem("todo", JSON.stringify(arrayTodo));
   console.log(JSON.parse(localStorage.getItem("todo")));
 }
+
+
 //pull/display (recorded information shown) (w/o parameters)
 function displayStorage() {
   if (JSON.parse(localStorage.getItem("todo"))) {
@@ -49,6 +74,7 @@ function displayStorage() {
   else console.log("Talia, local storage not found");
 }
 
+
 //every element with this class will call this function using jQuery (saveButton)
 $(document).ready(function() {
   $(".buttonColor").click(function(event) {
@@ -58,6 +84,7 @@ $(document).ready(function() {
     saveStorage(this.id, textInput);
   });
 });
+
 
 setDate();
 hourList();
